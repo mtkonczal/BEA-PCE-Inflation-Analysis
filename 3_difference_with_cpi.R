ce99071213b30c14e09799d5cd6bda5928d9d755
Mike Konczal <100174099+mtkonczal@users.noreply.github.com>
2023-06-29 20:09:33 -0400

    updated graphics
    Multiple updated graphics.

--- 3_difference_with_cpi.R
+++ 3_difference_with_cpi.R
@@ -3,10 +3,110 @@
 if(!exists("title_three_dashboard")){
 title_three_dashboard <- "This is a test showing how the PCE inflation breaks down default"}
 
+pce %>% filter(LineDescription == "PCE excluding food and energy")
 
-load("data/")
+#First make a graphic of non-housing services
+#### Basic Index ####
+nhs_index <-
+  pce %>%
+  select(date, LineDescription, WDataValue_P1, PCEweight) %>%
+  group_by(date) %>%
+  summarize(nhsWP1 = WDataValue_P1[LineDescription == "PCE services excluding energy"] - WDataValue_P1[LineDescription == "Housing"],
+            nhs_weight = PCEweight[LineDescription == "PCE services excluding energy"] - PCEweight[LineDescription == "Housing"],
+  ) %>%
+  ungroup() %>%
+  mutate(nhsWP1A = nhsWP1/nhs_weight) %>%
+  mutate(nhsWP1A = (nhsWP1A+1)^12-1) %>%
+  mutate(index = nhsWP1/nhs_weight+1) %>% filter(!is.na(index)) %>%
+  mutate(index = cumprod(index))
+#####
 
-#SET UP DATA:
+##### Graphic for Non-Housing Services ####
+nhs_index_1_month <- nhs_index %>%
+  mutate(month1 = (index/lag(index,1))^12-1) %>%
+  select(date, month1) %>% mutate(type="month3") %>%
+  filter(month1 > -0.03)
+
+nhs_index %>%
+  mutate(month3 = (index/lag(index,3))^4-1,
+         month6 = (index/lag(index,6))^2-1) %>%
+  select(date, month3, month6) %>%
+  filter(date >= "2017-01-01") %>%
+  pivot_longer(month3:month6, names_to = "type", values_to = "value") %>%
+  left_join(nhs_index_1_month, by=c("date","type")) %>%
+  mutate(last_value = ifelse(date == max(date), value, as.numeric(NA))) %>%
+  ggplot(aes(date, value, color=type, label=label_percent()(last_value))) + geom_line(size=1.2) +
+  labs(x="", y="",
+       title="I am a graphicTKKTKTKT of Non-Housing Services",
+       subtitle = "Tester",
+       caption = "April 2020 excluded on the 1-month. TKTKTKTAll items less food and energy, monthly percent change, BLS, Author's calculations. Mike Konczal, Roosevelt Institute.") +
+  theme_lass +
+#  geom_hline(yintercept = pre_core, linetype="dashed", color="#A4CCCC") +
+  scale_fill_brewer(palette="Paired") +
+  theme(panel.grid.major.y = element_line(size=0.5)) +
+  theme(plot.title.position = "plot") +
+  scale_y_continuous(labels = percent) +
+  scale_x_date(date_labels = "%b\n%Y") +
+  theme(legend.position = c(0.90,0.15), legend.text = element_text(size=15)) +
+  scale_color_manual(values=c("#2D779C", "#A4CCCC")) +
+  geom_text(show.legend=FALSE, nudge_x = 120) +
+  geom_col(aes(date, month1), alpha=0.1, size=0, show.legend = FALSE)
+
+ggsave("graphics/pce_nhs_inflation.png", dpi="retina", width = 12, height=6.75, units = "in")
+
+
+##### SuperCore ####
+#First make a graphic of non-housing services
+#### Basic Index ####
+supercore_index <-
+  pce %>%
+  select(date, LineDescription, WDataValue_P1, PCEweight) %>%
+  group_by(date) %>%
+  summarize(nhsWP1 = WDataValue_P1[LineDescription == "PCE excluding food and energy"] - WDataValue_P1[LineDescription == "Housing"] - WDataValue_P1[LineDescription == "Net purchases of used motor vehicles (56)"],
+            nhs_weight = PCEweight[LineDescription == "PCE excluding food and energy"] - PCEweight[LineDescription == "Housing"] - PCEweight[LineDescription == "Net purchases of used motor vehicles (56)"],
+  ) %>%
+  ungroup() %>%
+  mutate(nhsWP1A = nhsWP1/nhs_weight) %>%
+  mutate(nhsWP1A = (nhsWP1A+1)^12-1) %>%
+  mutate(index = nhsWP1/nhs_weight+1) %>% filter(!is.na(index)) %>%
+  mutate(index = cumprod(index))
+#####
+
+##### Graphic for Non-Housing Services ####
+supercore_index_1_month <- supercore_index %>%
+  mutate(month1 = (index/lag(index,1))^12-1) %>%
+  select(date, month1) %>% mutate(type="month3") %>%
+  filter(month1 > -0.03)
+
+supercore_index %>%
+  mutate(month3 = (index/lag(index,3))^4-1,
+         month6 = (index/lag(index,6))^2-1) %>%
+  select(date, month3, month6) %>%
+  filter(date >= "2017-01-01") %>%
+  pivot_longer(month3:month6, names_to = "type", values_to = "value") %>%
+  left_join(supercore_index_1_month, by=c("date","type")) %>%
+  mutate(last_value = ifelse(date == max(date), value, as.numeric(NA))) %>%
+  ggplot(aes(date, value, color=type, label=label_percent()(last_value))) + geom_line(size=1.2) +
+  labs(x="", y="",
+       title="I am a graphicTKKTKTKT of Supercore",
+       subtitle = "Tester",
+       caption = "April 2020 excluded on the 1-month. TKTKTKTAll items less food and energy, monthly percent change, BLS, Author's calculations. Mike Konczal, Roosevelt Institute.") +
+  theme_lass +
+  #  geom_hline(yintercept = pre_core, linetype="dashed", color="#A4CCCC") +
+  scale_fill_brewer(palette="Paired") +
+  theme(panel.grid.major.y = element_line(size=0.5)) +
+  theme(plot.title.position = "plot") +
+  scale_y_continuous(labels = percent) +
+  scale_x_date(date_labels = "%b\n%Y") +
+  theme(legend.position = c(0.90,0.15), legend.text = element_text(size=15)) +
+  scale_color_manual(values=c("red", "purple")) +
+  geom_text(show.legend=FALSE, nudge_x = 120) +
+  geom_col(aes(date, month1), alpha=0.1, size=0, show.legend = FALSE)
+
+ggsave("graphics/pce_nhs_inflation.png", dpi="retina", width = 12, height=6.75, units = "in")
+
+
+#### Set up CPI ####
 cpi <- cpi_data %>%
   filter(period != "M13") %>%
   filter(seasonal == "S") %>%
@@ -24,109 +124,136 @@ cpi <- cpi_data %>%
   mutate(Wchange12 = (Pchange12*weight)/100) %>%
   ungroup()
 
-
-core_cpi <- cpi %>% filter(item_name == "All items less food and energy") %>%
-  mutate(Pchange1a = (Pchange1+1)^12-1) %>%
-  select(date, core_cpi = Pchange1a)
-
-
-pce %>% filter(LineDescription == "PCE excluding food and energy") %>%
-  left_join(core_cpi, by="date") %>%
-  mutate(DataValue_P1a = (1+DataValue_P1)^12-1) %>%
-  mutate(diff = core_cpi - DataValue_P1a) %>%
-  group_by(year(date)) %>%
-  summarize(avg_diff = mean(diff))
-
-##### SERVICES
-
-pce %>% filter(LineDescription == "PCE excluding food and energy") %>%
-  left_join(core_cpi, by="date") %>%
-  mutate(DataValue_P1a = (1+DataValue_P1)^12-1) %>%
-  mutate(diff = core_cpi - DataValue_P1a) %>%
-  
-  ggplot(aes(date,diff)) + geom_line()
-
-core_services_cpi <- cpi %>% filter(item_name == "Services less energy services") %>%
-  mutate(Pchange1a = (Pchange1+1)^12-1) %>%
-  select(date, core_cpi = Pchange1a)
-
-pce %>% filter(LineDescription == "PCE services excluding energy") %>%
-  left_join(core_services_cpi, by="date") %>%
-  mutate(DataValue_P1a = (1+DataValue_P1)^12-1) %>%
-  mutate(diff = core_cpi - DataValue_P1a) %>%
-  
-  ggplot(aes(date,diff)) + geom_line()
-
-
-core_services_cpi <- cpi %>% filter(item_name == "Services less energy services") %>%
-  mutate(Pchange1a = (Pchange1+1)^12-1) %>%
-  select(date, core_cpi = Pchange1a)
-
-pce %>% filter(LineDescription == "Market-based PCE services") %>%
-  left_join(core_services_cpi, by="date") %>%
-  mutate(DataValue_P1a = (1+DataValue_P1)^12-1) %>%
-  mutate(diff = core_cpi - DataValue_P1a) %>%
-  
-  ggplot(aes(date,diff)) + geom_line()
-
-
-pce %>% filter(LineDescription == "PCE services excluding energy") %>%
-  left_join(core_services_cpi, by="date") %>%
-  mutate(DataValue_P1a = (1+DataValue_P1)^12-1) %>%
-  mutate(diff = core_cpi - DataValue_P1a) %>%
-  group_by(year(date)) %>%
-  summarize(avg_diff = mean(diff))
-
-
-
-
-core_goods_fields <- c("Goods","Gasoline and other energy goods","Food and beverages purchased for off-premises consumption","Services","Electricity and gas","Housing")
-
-core_analysis <- pce %>% filter(LineDescription %in% core_goods_fields) %>%
-  filter(date >= "2018-01-01") %>%
-  select(date, LineDescription, WDataValue_P1) %>%
-  pivot_wider(names_from=LineDescription, values_from = WDataValue_P1) %>%
-  clean_names() %>%
-  mutate(core_goods = goods - food_and_beverages_purchased_for_off_premises_consumption - gasoline_and_other_energy_goods) %>%
-  mutate(core_services = services - electricity_and_gas - housing) %>%
-  mutate(core_inflation = core_goods + core_services) %>%
-  pivot_longer(-date, names_to = "item_name", values_to = "WDataValue_P1") %>%
-  filter(item_name %in% c("core_goods","core_services","housing")) %>%
-  mutate(item_nameF = factor(item_name, levels = c("core_goods", "housing", "core_services")))
-
-levels(core_analysis$item_nameF)[levels(core_analysis$item_nameF) == "core_goods"] <-"Core Goods"
-levels(core_analysis$item_nameF)[levels(core_analysis$item_nameF) == "core_services"] <-"Core Services excluding Housing"
-levels(core_analysis$item_nameF)[levels(core_analysis$item_nameF) == "housing"] <-"Housing"
-
-pce_services_ex_housing <- core_analysis %>% filter(item_nameF == "Core Services excluding Housing") %>%
-  mutate(WDataValue_P1 = (1+WDataValue_P1)^12-1) %>%
-  select(date, item_name, value = WDataValue_P1) %>% mutate(type = "PCE", form="Core Services ex Housing")
-  
-  
-  
-  ###### Graphic 3: Services Breakdown #####
-cpi_services_ex_housing <- cpi %>% filter(date > "2017-12-01", item_name %in% c("Services less energy services", "Shelter")) %>%
+cpi_nhs <- cpi %>% filter(date > "2020-12-01", item_name %in% c("Services less energy services", "Shelter")) %>%
   select(date, item_name, Wchange1a) %>%
   pivot_wider(names_from = item_name, values_from = Wchange1a) %>%
   mutate(`Rest of core services` = `Services less energy services` - `Shelter`) %>%
   select(-`Services less energy services`) %>%
   pivot_longer(c(Shelter, `Rest of core services`), names_to = "item_name", values_to = "Wchange1a") %>%
-  filter(item_name == "Rest of core services") %>%
-  select(date, item_name, value = Wchange1a) %>% mutate(type = "CPI", form="Core Services ex Housing")
+  filter(item_name == "Rest of core services")
 
-cpi_services_diff <- pce_services_ex_housing %>% rename(value_pce = value) %>% left_join(cpi_services_ex_housing, by="date") %>%
-  mutate(diff = value_pce - value) %>%
-  select(date, item_name = item_name.x, value = diff) %>% mutate(type = "PCE minus CPI Value", form="PCE minus CPI Value, Core Services ex Housing")
 
-rbind(pce_services_ex_housing, cpi_services_ex_housing, cpi_services_diff) %>%
-  ggplot(aes(date,value,color=type)) + geom_line() + theme_lass + facet_wrap(~form) +
-  theme(legend.position = "bottom") +
-  scale_y_continuous(labels = percent) +
-  labs(title="How Divergent Has Core Services ex Housing Been Between CPI and PCE?",
-       subtitle="Monthly Weighted Contribution to Inflation, Annualized",
-       caption="PRELIMINARY: PCE excludes 'Housing', CPI excludes 'Shelter', PCE weights are nominal consumption shares as a percent of total spending, Mike Konczal, Roosevelt Institute")
+cpi_nhs_index <-
+  cpi %>%
+  mutate(weight_nhs = if_else(!is.na(weight_2023), weight_2023, weight)) %>%
+  select(date, item_name, Wchange1, weight_nhs) %>%
+  mutate(weight_nhs = weight_nhs/100) %>%
+  group_by(date) %>%
+  summarize(nhsWP1 = Wchange1[item_name == "Services less energy services"] - Wchange1[item_name == "Shelter"],
+            nhs_weight = weight_nhs[item_name == "Services less energy services"] - weight_nhs[item_name == "Shelter"],
+  ) %>%
+  ungroup() %>%
+  mutate(nhsWP1A = nhsWP1/nhs_weight) %>%
+  mutate(nhsWP1A = (nhsWP1A+1)^12-1) %>%
+  mutate(index = nhsWP1/nhs_weight+1) %>% filter(!is.na(index)) %>%
+  mutate(index = cumprod(index), type = "cpi")
+#### Make difference ####
+
+
+#### Attempt One ####
+nhs_index_1 <-
+  pce %>%
+  select(date, LineDescription, WDataValue_P1, PCEweight) %>%
+  group_by(date) %>%
+  summarize(nhsWP1 = WDataValue_P1[LineDescription == "PCE services excluding energy"] - WDataValue_P1[LineDescription == "Housing"],
+            nhs_weight = PCEweight[LineDescription == "PCE services excluding energy"] - PCEweight[LineDescription == "Housing"],
+            nhs_excluding_finservices = nhsWP1 - WDataValue_P1[LineDescription == "Financial services furnished without payment (107)"],
+            nhs_w_excluding_finservices = nhs_weight - PCEweight[LineDescription == "Financial services furnished without payment (107)"],
+            nhs_ex_fin_hos = nhs_excluding_finservices - WDataValue_P1[LineDescription == "Hospital and nursing home services"],
+            nhs_ex_fin_hos_w = nhs_w_excluding_finservices - PCEweight[LineDescription == "Hospital and nursing home services"],
+            nhs_ex_fin_hos_food = nhs_ex_fin_hos - WDataValue_P1[LineDescription == "Food services"],
+            nhs_ex_fin_hos_food_w = nhs_ex_fin_hos_w - PCEweight[LineDescription == "Food services"],
+            
+  ) %>%
+  ungroup() %>%
+  mutate(nhsWP1 = nhsWP1/nhs_weight) %>%
+  select(-nhs_weight) %>%
   
-ggsave("graphics/divergence_q.png", dpi="retina", width = 12, height=6.75, units = "in")
+  mutate(nhs_excluding_finservices = nhs_excluding_finservices/nhs_w_excluding_finservices) %>%
+  select(-nhs_w_excluding_finservices) %>%
+  
+  mutate(nhs_ex_fin_hos = nhs_ex_fin_hos/nhs_ex_fin_hos_w) %>%
+  select(-nhs_ex_fin_hos_w) %>%
+  
+  mutate(nhs_ex_fin_hos_food = nhs_ex_fin_hos_food/nhs_ex_fin_hos_food_w) %>%
+  select(-nhs_ex_fin_hos_food_w) %>%
+  
+  filter(!is.na(nhs_excluding_finservices), date > "2019-01-01") %>%
+  pivot_longer(cols =c("nhsWP1","nhs_excluding_finservices","nhs_ex_fin_hos","nhs_ex_fin_hos_food"), names_to = "type", values_to = "value") %>%
+  mutate(value = (value+1)^12-1) %>%
+  filter(date > "2022-05-01") %>%
+  ggplot(aes(date,value,color=type)) + geom_line() + theme_classic() + theme(legend.position = "bottom") +
+  geom_line(data=cpi_nhs_index[cpi_nhs_index$date > "2022-05-01",], aes(date, nhsWP1A), color="blue",
+            linetype="dotted", linesize=1.2)
+
+
+mutate(nhsWP1A = (nhsWP1A+1)^12-1) %>%
+  mutate(index = nhsWP1/nhs_weight+1) %>% filter(!is.na(index)) %>%
+  mutate(index = cumprod(index))
+
+
+ggsave("graphics/nhs_difference_pce_cpi.png", dpi="retina", width = 12, height=6.75, units = "in")
 
 
 
+##### Quarterly Check #####
+# Table IDs
+# https://www.bea.gov/system/files/2021-07/TablesRegisterPreview.txt
+
+cpi_diff <- get_NIPA_data(beaKey, 'U90100', 'Q', '2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023', data_set_name = 'NIUnderlyingDetail')
+cpi_diff <- BEA_date_quarterly(cpi_diff)
+#cpi_diff <- BEA_date_monthly(cpi_diff)
+
+diff_high <- c("PCE Chain-type price index (percent change)","Equals: CPI (percent change)","Rent of shelter")
+
+diff_types <- c("Less: Weight effect (percentage points)",
+          "Less: Scope effect - PCE price index items out-of-scope of the CPI (percentage points)",
+          "Plus: Scope effect - CPI items out-of-scope of the PCE price index (percentage points)",
+          "Less: Other effects (percentage points)"
+          )
+
+cpi_diff %>% filter(LineDescription %in% diff_types) %>%
+  ggplot(aes(date,DataValue, color=LineDescription)) + geom_line(size=1.2) +
+  theme_classic() + theme(legend.position = "bottom") + facet_wrap(~LineDescription, scales = "free")
+
+cpi_diff
+
+cpi_housing_diff <-
+  cpi_diff %>% filter(LineDescription %in% diff_high) %>%
+  group_by(date) %>%
+  mutate(DataValue = if_else(LineDescription=="Rent of shelter",
+                             DataValue[LineDescription=="PCE Chain-type price index (percent change)"]-DataValue[LineDescription=="Rent of shelter"], DataValue)) %>%
+  ungroup() %>%
+  ggplot(aes(date,DataValue, color=LineDescription)) + geom_line(size=1.2) +
+  theme_classic() + theme(legend.position = "bottom") + facet_wrap(~LineDescription)
+
+
+cpi_diff %>% group_by(date) %>%
+  summarize(pce = DataValue[LineDescription == "PCE Chain-type price index (percent change)"],
+            less_weight = pce + DataValue[LineDescription == "Less: Weight effect (percentage points)"],
+            less_weight_scope = less_weight + DataValue[LineDescription=="Less: Scope effect - PCE price index items out-of-scope of the CPI (percentage points)"] + DataValue[LineDescription=="Plus: Scope effect - CPI items out-of-scope of the PCE price index (percentage points)"],
+            cpi = DataValue[LineDescription == "Equals: CPI (percent change)"])
+
+
+
+
+cpi_diff %>% filter(LineDescription == "Seasonal adjustment") %>%
+  mutate(DataValue = DataValue/100) %>%
+  ggplot(aes(date,DataValue)) + geom_line(size=1.2) + theme_classic() +
+  theme(legend.position = "bottom", plot.title.position = "plot") +
+  labs(subtitle="Differences in seasonal adjustment methodology between CPI and PCE is having larger effects. Quarterly, Annualized.",
+       caption="BEA: Table 9.1U. Reconciliation of Percent Change in the CPI with Percent Change in the PCE Price Index, Mike Konczal, Roosevelt Institute",
+       y="Difference between CPI and PCE",x=NULL) +
+  scale_y_continuous(labels = percent) +
+  scale_x_date(date_labels = "%b\n%Y")
+
+ggsave("graphics/diff.png", dpi="retina", width = 12, height=6.75, units = "in")
+
+
+
+
+cpi_diff %>% filter(LineDescription == "Seasonal adjustment") %>%
+  mutate(year = year(date)) %>%
+  group_by(year) %>%
+  summarize(addded = sum(DataValue)) %>%
+  ggplot(aes(year,addded)) + geom_line()
--- cpi_pce_mapping/mapping_exercise.R
+++ cpi_pce_mapping/mapping_exercise.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(janitor)
-
+library(lubridate)
+library(scales)
 
 load("data/last_cpi_data.RData")
 load("data/pce_long.RData")
@@ -34,7 +35,21 @@ cpi_nhs <- cpi %>% filter(date > "2020-12-01", item_name %in% c("Services less e
   pivot_longer(c(Shelter, `Rest of core services`), names_to = "item_name", values_to = "Wchange1a") %>%
   filter(item_name == "Rest of core services")
 
-
+cpi_nhs_index <-
+  cpi %>%
+  mutate(weight_nhs = if_else(!is.na(weight_2023), weight_2023, weight)) %>%
+  select(date, item_name, Wchange1, weight_nhs) %>%
+  mutate(weight_nhs = weight_nhs/100) %>%
+  group_by(date) %>%
+  summarize(nhsWP1 = Wchange1[item_name == "Services less energy services"] - Wchange1[item_name == "Shelter"],
+            nhs_weight = weight_nhs[item_name == "Services less energy services"] - weight_nhs[item_name == "Shelter"],
+  ) %>%
+  ungroup() %>%
+  mutate(nhsWP1A = nhsWP1/nhs_weight) %>%
+  mutate(nhsWP1A = (nhsWP1A+1)^12-1) %>%
+  mutate(index = nhsWP1/nhs_weight+1) %>% filter(!is.na(index)) %>%
+  mutate(index = cumprod(index), type = "cpi")
+#####
 
 #### Basic Index ####
 nhs_index <-
@@ -43,8 +58,6 @@ nhs_index <-
   group_by(date) %>%
   summarize(nhsWP1 = WDataValue_P1[LineDescription == "PCE services excluding energy"] - WDataValue_P1[LineDescription == "Housing"],
             nhs_weight = PCEweight[LineDescription == "PCE services excluding energy"] - PCEweight[LineDescription == "Housing"],
-            nhs_excluding_finservices = nhsWP1 - WDataValue_P1[LineDescription == "Financial services furnished without payment (107)"],
-            nhs_w_excluding_finservices = nhs_weight - PCEweight[LineDescription == "Financial services furnished without payment (107)"],
   ) %>%
   ungroup() %>%
   mutate(nhsWP1A = nhsWP1/nhs_weight) %>%
@@ -85,8 +98,10 @@ nhs_index_1 <-
   filter(!is.na(nhs_excluding_finservices), date > "2019-01-01") %>%
   pivot_longer(cols =c("nhsWP1","nhs_excluding_finservices","nhs_ex_fin_hos","nhs_ex_fin_hos_food"), names_to = "type", values_to = "value") %>%
   mutate(value = (value+1)^12-1) %>%
+  filter(date > "2022-05-01") %>%
   ggplot(aes(date,value,color=type)) + geom_line() + theme_classic() + theme(legend.position = "bottom") +
-  geom_line(data=cpi_nhs, aes(date, Wchange1a), color="purple")
+  geom_line(data=cpi_nhs_index[cpi_nhs_index$date > "2022-05-01",], aes(date, nhsWP1A), color="blue",
+            linetype="dotted", linesize=1.2)
   
   
   mutate(nhsWP1A = (nhsWP1A+1)^12-1) %>%
@@ -95,9 +110,15 @@ nhs_index_1 <-
 
 nhs_index %>%
   mutate(six_change = (index/lag(index,6))^2-1) %>%
-  ggplot(aes(date,six_change)) + geom_line() + theme_classic()
+  ggplot(aes(date,six_change)) + geom_line() + theme_classic() +
+  scale_y_continuous(labels = percent) +
+  labs(y="", subtitle="PCE non-housing services, six-month change, annualized. Weights approximated as nominal consumption shares as a percent of the total.",
+       caption="NIPA Tables 2.4.4U and 2.4.5U. Mike Konczal",
+       title="Wow, That's Crazy") +
+  theme(plot.title.position = "plot")
 
 
+ggsave("long_nhs.png", dpi="retina", width = 12, height=6.75, units = "in")
 
 
 
@@ -127,7 +148,7 @@ tail(cpi_data %>% filter(item_name == "Pork", seasonal == "S") %>% select(date, 
 
 mapping %>% filter(pce_description == "Pork")
 
-pce[grepl("hysic", pce$LineDescription),]
+pce[grepl("food and energy", pce$LineDescription),]$LineDescription
 
 b %>% filter(item_name == "Pork")
 mapping %>% filter(pce_description == "Pork")
@@ -140,3 +161,64 @@ tail(pce %>% filter(LineDescription == "Pork") %>% select(date, LineDescription,
 tail(cpi_data %>% filter(item_name == "Pork", seasonal == "S") %>% select(date, item_name, value) %>% mutate(change = value/lag(value,1)-1))
 tail(pce %>% filter(LineDescription == "Physician services (44)") %>% select(date, LineDescription, DataValue_P1))
 tail(cpi_data %>% filter(item_name == "Physicians' services", seasonal == "S") %>% select(date, item_name, value) %>% mutate(change = value/lag(value,1)-1))
+
+
+#### Airlines ####
+pce[grepl("Food", pce$LineDescription),] %>% select(LineDescription) %>% distinct()
+
+cpi_air <-
+cpi_data %>% filter(item_name == "Airline fares", seasonal == "S") %>%
+  mutate(change = value/lag(value,1)-1, type = "cpi") %>%
+  select(date, change, type)
+
+pce_air <-
+  pce %>% filter(LineDescription == "Air transportation (64)") %>%
+  mutate(type = "pce") %>%
+  select(date, change = DataValue_P1, type)
+
+
+pce %>% filter(LineDescription %in% c("Food services","Food and beverages purchased for off-premises consumption")) %>%
+  filter(year(date) > 2015) %>%
+  mutate(DataValue_P1 = (DataValue_P1+1)^12-1) %>%
+  ggplot(aes(date,DataValue_P1, color=LineDescription)) + geom_line() +
+  theme_classic() + theme(legend.position = "bottom") +
+  geom_hline(yintercept = 0)
+
+
+##### First graphic ####
+cpi_core <- cpi_data %>% filter(item_name == "All items less food and energy", seasonal == "S") %>%
+  mutate(diff = value/lag(value,1), diff = diff^12-1) %>%
+  select(date, diff) %>% mutate(type = "cpi")
+
+pce_core <-
+  pce %>% filter(LineDescription == "PCE excluding food and energy") %>%
+  select(date, diff = DataValue_P1) %>%
+  mutate(diff = (diff+1)^12-1, type = "pce")
+
+rbind(cpi_core, pce_core) %>%
+  filter(year(date)>2014) %>%
+  ggplot(aes(date,diff,color=type)) + geom_line() + theme_classic() +
+  theme(legend.position = "bottom")
+
+#####
+nhs_index %>% mutate(type = "pce") %>%
+  rbind(cpi_nhs_index) %>%
+  filter(year(date)>2014) %>%
+  ggplot(aes(date,nhsWP1A,color=type)) + geom_line() + theme_classic() +
+  theme(legend.position = "bottom")
+
+nhs_index %>% mutate(type = "pce") %>%
+  rbind(cpi_nhs_index) %>%
+  filter(year(date)>2014) %>%
+  group_by(type) %>%
+  mutate(threemonths = index/lag(index,3), threemonths = threemonths^4-1) %>%
+  ungroup() %>%
+  ggplot(aes(date,threemonths,color=type)) + geom_line() + theme_classic() +
+  theme(legend.position = "bottom")
+####
+
+tail(cpi %>% filter(item_name == "Food away from home") %>% select(date, item_name, Wchange1a))
+
+
+#### This ####
+
