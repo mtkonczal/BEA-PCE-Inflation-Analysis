9c524e2d2ba577a87d808ee96f60d721e33ad86b
Mike Konczal <100174099+mtkonczal@users.noreply.github.com>
2023-09-04 17:56:49 -0400

    Updates

--- supply_demand.R
+++ supply_demand.R
@@ -8,10 +8,10 @@ library(hrbrthemes)
 setwd("/Users/mkonczal/Documents/GitHub/BEA-PCE-Inflation-Analysis/")
 beaKey <- read_csv("/Users/mkonczal/Documents/data_folder/BEA_key/BEA_key.csv")
 beaKey <- as.character(beaKey)
+
 source("1a_helper_functions.R")
-#source("1b_load_PCE_items_all.R")
-#source("1b_load_PCE_items.R")
-load("data/pce_long.RData")
+source("1b_load_PCE_items.R")
+#load("data/pce_supply_demand.RData")
 # Table IDs
 # https://www.bea.gov/system/files/2021-07/TablesRegisterPreview.txt
 
@@ -30,9 +30,10 @@ remove_outliers <- function(x, multiplier = 1.5) {
 # Get preloaded categories and levels for each
 lowest <- read_csv("data/pce_items_lowest.csv")
 
-months_change <- 3
+months_change <- 6
 compare_date <- "2022-12-01"
-#compare_date <- as.Date("2022-06-01")
+compare_date <- max(pce$date) %m-% months(months_change+1)
+#compare_date <- as.Date("2022-07-01")
 
 recent <-pce %>% group_by(LineDescription) %>% filter(LineDescription != 323 | LineDescription != 324) %>%
   mutate(QuantityFinal = Quantity/lag(Quantity,months_change)-1,
@@ -51,8 +52,8 @@ recent %>% inner_join(lowest,by="LineDescription") %>% filter(category != "Aggre
   mutate(x = LineDescription == lag(LineDescription)) %>%
   #filter(!x) %>%
   filter(level == "Level 4") %>%
-  #mutate(QuantityFinal = remove_outliers(QuantityFinal, 3)) %>%
-  #mutate(PriceFinal = remove_outliers(PriceFinal, 3)) %>%
+  mutate(QuantityFinal = remove_outliers(QuantityFinal, 3)) %>%
+  mutate(PriceFinal = remove_outliers(PriceFinal, 3)) %>%
   ggplot(aes(QuantityFinal,PriceFinal,size=weight, color=category)) + geom_point(aes(fill="skyblue"), alpha=0.5, shape = 21, color = "black", stroke = 1.5, show.legend=FALSE) +
   theme_classic() + geom_hline(yintercept = 0) + geom_vline(xintercept = 0) +
   labs(subtitle = "3-month Change July 2023 Minus 3-month Change December 2022, Quantity and Inflation, for ~130 Core PCE Item Categories",
@@ -68,6 +69,16 @@ recent %>% inner_join(lowest,by="LineDescription") %>% filter(category != "Aggre
 
 ggsave("graphics/supply_demand.png", dpi="retina", width = 12, height=6.75, units = "in")
 
+
+pce_sd <- pce %>% group_by(LineDescription) %>% filter(LineDescription != 323 | LineDescription != 324) %>%
+  filter(year(date)<2020) %>%
+  mutate(QuantityFinal = Quantity/lag(Quantity,months_change)-1,
+         QuantityFinal = QuantityFinal - lag(QuantityFinal,7)
+  ) %>%
+  na.omit() %>%
+  summarize(sd_quantity = sd(QuantityFinal)) %>% ungroup()
+
+b <-
 recent %>% inner_join(lowest,by="LineDescription") %>%
   filter(category != "Aggregate") %>%
   mutate(category = if_else(category %in% c("Energy","Food"), "Food and Energy",category)) %>%
@@ -75,6 +86,7 @@ recent %>% inner_join(lowest,by="LineDescription") %>%
   mutate(x = LineDescription == lag(LineDescription)) %>%
   filter(!x) %>%
   filter(level == "Level 4") %>%
+  #left_join(pce_sd, by="LineDescription") %>%
   mutate(quadrant = case_when(
     QuantityFinal > 0 & PriceFinal > 0 ~ "Quadrant 1 - Demand+",
     QuantityFinal < 0 & PriceFinal > 0 ~ "Quadrant 2 - Supply-",
@@ -85,11 +97,15 @@ recent %>% inner_join(lowest,by="LineDescription") %>%
   summarize(n = sum(weight), nW = sum(weight*PriceFinal)) %>%
   ungroup() %>%
   group_by(category) %>%
-  mutate(n = n/sum(n)) %>%
+  mutate(Sn = n/sum(n), SnW = nW/sum(nW)) %>%
   ungroup() %>%
   arrange(category)
 
 
+c <- b %>% filter(category != "Food and Energy") %>% select(-n,-nW) %>%
+  pivot_wider(names_from = category, values_from = c(Sn,SnW))
+
+b %>% group_by(category) %>% summarize(sum(nW))
 ### assume we have shapiro loaded  - cyclical_categories
 
 cyclical_categories <- c("Accessories and parts",
@@ -125,10 +141,10 @@ cyclical_categories <- c("Accessories and parts",
 
 recent %>% 
   filter(LineDescription %in% cyclical_categories) %>%
-  
+  inner_join(lowest,by="LineDescription") %>%
 #  mutate(QuantityFinal = remove_outliers(QuantityFinal, 3)) %>%
 #  mutate(PriceFinal = remove_outliers(PriceFinal, 3)) %>%
-  ggplot(aes(QuantityFinal,PriceFinal,size=weight)) + geom_point(aes(fill="skyblue"), alpha=0.5, shape = 21, color = "black", stroke = 1.5, show.legend=FALSE) +
+  ggplot(aes(QuantityFinal,PriceFinal,size=weight,fill=category)) + geom_point(alpha=0.5, shape = 21, color = "black", stroke = 1.5, show.legend=FALSE) +
   theme_classic() + geom_hline(yintercept = 0) + geom_vline(xintercept = 0) +
   labs(subtitle = "3-month Change June 2023 Minus 3-month Change June 2022, Quantity and Inflation, for ~130 Core PCE Item Categories",
        title="Deceleration is Driven by Expanded Supply",
@@ -137,11 +153,33 @@ recent %>%
        x = "Percentage Point Change in Quantity (Real Value)") +
   scale_y_continuous(labels = scales::percent) +
   scale_x_continuous(labels = scales::percent) +
-  theme(plot.title.position = "plot", legend.position = c(0.85,0.9)) +
+  theme(plot.title.position = "plot", legend.position = "bottom") +
   scale_size_continuous(range = c(3, 10))
 
 recent %>% filter(LineDescription %in% cyclical_housing_categories)
 
+
+b <-
+  recent %>% inner_join(lowest,by="LineDescription") %>%
+  mutate(category = if_else(category %in% c("Energy","Food"), "Food and Energy",category)) %>%
+  filter(LineDescription %in% cyclical_categories) %>%
+  #left_join(pce_sd, by="LineDescription") %>%
+  mutate(quadrant = case_when(
+    QuantityFinal > 0 & PriceFinal > 0 ~ "Quadrant 1 - Demand+",
+    QuantityFinal < 0 & PriceFinal > 0 ~ "Quadrant 2 - Supply-",
+    QuantityFinal < 0 & PriceFinal < 0 ~ "Quadrant 3 - Demand-",
+    QuantityFinal > 0 & PriceFinal < 0 ~ "Quadrant 4 - Supply+",
+    TRUE ~ "Undefined")) %>%
+  group_by(quadrant) %>%
+  summarize(n = sum(weight), nW = sum(weight*PriceFinal)) %>%
+  ungroup() %>%
+  mutate(Sn = n/sum(n), SnW = nW/sum(nW))
+
+
+c <- b %>% filter(category != "Food and Energy") %>% select(-n,-nW) %>%
+  pivot_wider(names_from = category, values_from = c(Sn,SnW))
+
+b %>% group_by(category) %>% summarize(sum(nW))
 
 #### Historical dive ####
 
